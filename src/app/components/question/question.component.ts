0ac4ef377d6588e9be694f550e6e7b285d685e6a
Vitris <tug24657@temple.edu>
2017-12-12 19:53:13 -0500

    fin

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {DataService} from '../../services/data.service';
 
 import {Question} from '../../models/Question';
 @Component({
@@ -9,9 +10,15 @@ import {Question} from '../../models/Question';
 export class QuestionComponent implements OnInit {
   @Input('question') question:Question;
   
-  constructor() { }
+  constructor(public dataService:DataService) { 
+    
+  }
 
   ngOnInit() {
+  }
+
+  removeQuestion(question){
+    this.dataService.removeQuestion(question);
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,26 +6,67 @@ export class DataService {
   questions:Question[];
 
   constructor() { 
-    this.questions = [
-      {
-        text:'What is your name?',
-        answer: 'My name is Jack',
-        hide: true
-      },
-      {
-        text:'What is your fav color?',
-        answer: 'My green',
-        hide: true
-      },
-      {
-        text:'Who is Jacks bestfriend?',
-        answer: 'Nicole Gormley',
-        hide: true
-      }
-    ];
+    // this.questions = [
+    //   {
+    //     text:'What is your name?',
+    //     answer: 'My name is Jack',
+    //     hide: true
+    //   },
+    //   {
+    //     text:'What is your fav color?',
+    //     answer: 'My green',
+    //     hide: true
+    //   },
+    //   {
+    //     text:'Who is Jacks bestfriend?',
+    //     answer: 'Nicole Gormley',
+    //     hide: true
+    //   }
+    // ];
   }
 
+  //get questions from local storage
   getQuestions(){
+    if(localStorage.getItem('questions') === null){
+      this.questions = [];
+    }else {
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
     return this.questions;
+  }
+  //add questions to local storage
+  addQuestion(question:Question){
+    this.questions.unshift(question);
+
+    //Init local variable
+    let questions;
+
+    if(localStorage.getItem('questions') === null){
+      questions = [];
+
+      //push new question
+      questions.unshift(question);
+
+      //set new array to local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }else {
+      questions = JSON.parse(localStorage.getItem('questions'));
+
+      //Add new question
+      questions.unshift(question);
+
+      //Reset local storage
+      localStorage.setItem('questions', JSON.stringify(questions));
+    }
+    
+  }
+
+  removeQuestion(question){
+    for(let i = 0; i< this.questions.length; i++){
+      if(question == this.questions[i]){
+        this.questions.splice(i, 1);
+        localStorage.setItem('questions', JSON.stringify(this.questions));
+      }
+    }
   }
 }
